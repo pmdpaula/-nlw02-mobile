6b1dfc631127fe29464016b176403456aece8fd3
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-08-07 10:33:06 -0300

    Landing page pronta

--- App.tsx
+++ App.tsx
@@ -1,15 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { Fragment } from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+
 import Landing from './src/pages/Landing';
 
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
 export default function App() {
-  return (
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } 
+  else {
+    return(
     <>
       <Landing />
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </>
-  );
+    )
+  }
 }
 
 const styles = StyleSheet.create({
