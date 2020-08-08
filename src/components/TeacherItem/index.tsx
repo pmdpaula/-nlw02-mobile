a278b997ed70ba9f3b7a020c06c2480d37bb3372
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-08-07 21:20:30 -0300

    Páginas internas com form de filtro.

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unFavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
+
+const TeacherItem = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profile}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://github.com/pmdpaula.png' }}
+        />
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Pedro Machado de Paula</Text>
+          <Text style={styles.subject}>História</Text>
+        </View>
+      </View>
+
+      <Text style={styles.bio}>
+        Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis.
+        {'\n'}{'\n'}
+        Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma pindureta.
+      </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'   '}
+          <Text style={styles.priceValue}>R$ 120,00</Text>
+        </Text>
+
+        <View style={styles.buttonContainer}>
+          <RectButton style={[styles.favoriteButton, styles.favorited]}>
+            {/* <Image source={heartOutlineIcon} /> */}
+            <Image source={unFavoriteIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entrar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+export default TeacherItem;
