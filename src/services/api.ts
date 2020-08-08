0a3a6a2d0a3dd83189fbc18b2ad169cec5848273
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-08-08 20:01:34 -0300

    api IP

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://172.28.112.77:3333'
+  baseURL: 'http://192.168.50.20:3333'
+  // baseURL: 'http://192.168.15.2:3333'
 })
 
 export default api;
