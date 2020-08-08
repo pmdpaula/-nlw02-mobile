ba168c2a102da8be13b9bfad5173703a58b51a30
Pedro Machado de Paula <pmdpaula@yahoo.com.br>
2020-08-08 00:38:06 -0300

    TotalConnections lendo do server

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Image, Text } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
-
+import api from '../../services/api';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -14,6 +14,16 @@ import styles from './styles';
 
 function Landing() {
   const { navigate } = useNavigation();
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections')
+      .then((response) => {
+        const { total } = response.data;
+        setTotalConnections(total);
+      });
+  }, []);
+
 
   function handleNavigateToGiveClassesPage() {
     navigate('GiveClasses');
@@ -51,7 +61,7 @@ function Landing() {
       </View>
 
       <Text style={styles.totalConnections}>
-        Total de 285 conexões já realizadas {' '}
+        Total de {totalConnections} conexões já realizadas {' '}
         <Image source={heartIcon} />
       </Text>
     </View>
